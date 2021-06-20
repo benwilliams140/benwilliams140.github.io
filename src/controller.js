a2c111253595eaf57ae69c6a119df95208c37891
Ben Williams <ben.williams14@outlook.com>
2021-06-20 18:04:38 -0400

    Added 3D rendering of cube

---
+++ src/controller.js
@@ -0,0 +1,21 @@
+const Three = require("./three");
+
+const scene = new Three.Scene();
+const camera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerWidth, 0.1, 1000);
+const renderer = new Three.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new Three.BoxGeometry();
+const material = new Three.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new Three.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+animate();
