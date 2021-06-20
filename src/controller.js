cf79427ad8427eb69ef506158d0b7b506d6bb157
Ben Williams <ben.williams14@outlook.com>
2021-06-20 18:08:14 -0400

    Fix

--- src/controller.js
+++ src/controller.js
@@ -1,15 +1,13 @@
-const Three = require("./three");
-
-const scene = new Three.Scene();
-const camera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerWidth, 0.1, 1000);
-const renderer = new Three.WebGLRenderer();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerWidth, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new Three.BoxGeometry();
-const material = new Three.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new Three.Mesh( geometry, material );
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
