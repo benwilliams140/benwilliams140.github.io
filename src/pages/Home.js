c0c06bb7f38180b4f250430d9c116a9ae615c476
Ben Williams <ben.williams14@outlook.com>
2021-12-26 22:53:27 -0500

    Updated to use padding instead of fixed positions,started working on landing page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,24 +4,30 @@ import Box from '../components/Box';
 
 function Home(props) {
     return(
-        <div className='bg-slate-400 bg-cover h-screen'>
-            <div className='bg-slate-500 bg-cover sm:rounded-r-full box-content fixed left-0 top-1/4 w-full sm:w-7/12 h-20 sm:h-40'>
-                <h1 className='text-slate-300 text-4xl sm:text-8xl font-bold tracking-widest leading-loose sm:leading-normal'>
+        <div className='bg-slate-400 bg-cover h-screen pt-52'>
+            <div className='bg-slate-500 bg-cover sm:rounded-r-full w-full pt-3 sm:pt-6 pb-0 sm:min-w-max sm:w-7/12 h-18 sm:h-40'>
+                <h1 className='text-slate-300 text-4xl sm:text-8xl font-bold tracking-widest'>
                     Benjamin Williams
                 </h1>
-                <p className='text-slate-300 text-sm sm:text-2xl font-bold tracking-widest absolute left-1/4 bottom-1'>Welcome to my personal sandbox.</p>
+                <p className='text-slate-300 text-sm sm:text-2xl font-bold tracking-widest pl-24 sm:pl-60 py-0'>Welcome to my personal sandbox.</p>
             </div>
-            <div className='w-full fixed bottom-8 sm:bottom-5'>
-                <Canvas>
-                    <ambientLight />
-                    <pointLight position={[10,10,10]} />
-                    <Box position={[0,0,0]}/>
-                </Canvas>
+            <div className='pt-2 sm:pt-4 fixed left-0'>
+                <p className='transition text-slate-300 bg-slate-500 bg-cover bg-clip-content text-xl rounded-r-full hover:scale-150 hover:translate-x-7 hover:font-bold duration-300 pt-2'>Who am I?</p>
+                <p className='transition text-slate-300 bg-slate-500 bg-cover bg-clip-content text-xl rounded-r-full hover:scale-150 hover:translate-x-7 hover:font-bold duration-300 pt-2'>What is this?</p>
             </div>
-            <div className='bg-slate-500 bg-cover rounded-t-lg fixed left-0 sm:left-1/4 bottom-0 w-full sm:w-1/2'>
-                <p className='text-slate-300 text-xl sm:text-2xl w-full text-center tracking-wider'>
-                    This is currently a WIP. Please see my <a className='underline' href='https://github.com/benwilliams140'>GitHub</a> profile.
-                </p>
+            <div className='content-center'>
+                <div className='w-full fixed bottom-8 sm:bottom-5'>
+                    <Canvas>
+                        <ambientLight />
+                        <pointLight position={[10,10,10]} />
+                        <Box position={[0,0,0]}/>
+                    </Canvas>
+                </div>
+                <div className='bg-slate-500 bg-cover rounded-t-lg fixed left-0 sm:left-1/4 bottom-0 w-full sm:min-w-max sm:w-1/2'>
+                    <p className='text-slate-300 text-xl sm:text-2xl w-full text-center tracking-wider'>
+                        This is currently a WIP. Please see my <a className='underline' href='https://github.com/benwilliams140'>GitHub</a> profile.
+                    </p>
+                </div>
             </div>
         </div>
     );
