8b534f8c2c45a754f751f79092de2abf47ad834e
Ben Williams <ben.williams14@outlook.com>
2023-12-04 22:08:41 -0500

    remove everything :(

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Sidebar, SidebarItem } from './Sidebar';
-export {
-  default as ScrollablePanel,
-  ScrollablePanelItem,
-} from './ScrollablePanel';
-export { default as ProjectCard } from './ProjectCard';
