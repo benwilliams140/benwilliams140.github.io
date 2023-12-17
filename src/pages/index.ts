b441959ff30afa22dec8a226def3fa37520b4e64
Ben Williams <ben.williams14@outlook.com>
2023-12-16 22:36:39 -0500

    remove everything :(

--- src/pages/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as AboutPage } from './About/About';
-export { default as ContactPage } from './Contact/Contact';
-export { default as HomePage } from './Home/Home';
-export { default as ProjectsPage } from './Projects/Projects';
